0bf86dc92fe33218788cbb4bb17de704f8387929
leopisani10 <leopisani@gmail.com>
2025-08-10 22:04:21 -0300

    Fix Puppeteer browser executable path

--- acasa-wa-bot/index.js
+++ acasa-wa-bot/index.js
@@ -222,6 +222,7 @@ if (HUB_TOKEN) {
     }),
     puppeteer: {
       headless: true,
+      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
       args: [
         '--no-sandbox',
         '--disable-setuid-sandbox',
